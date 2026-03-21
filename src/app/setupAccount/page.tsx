6e23d1521b583ca6baf16a22f340b2ba45817c33
Jeffrey Granados <jeffreygranados11@gmail.com>
2026-03-20 22:36:16 -0600

    feat: setupAccount/first step

---
+++ src/app/setupAccount/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import FirstStage from "./firstStage/page";
+
+export default function SetupAccount() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen gap-6">
+            <div className="flex flex-col items-start w-150 h-35.25 gap-1">
+                <p className="uppercase text-[14px] font-bold text-[#BCED09]">onboarding phase</p>
+                <h1 className="text-[36px] font-bold text-[#F1F5F9]">Account Setup</h1>
+                <h2 className="text-[#94A3B8] text-[18px]">Configure your professional DeFi trading environment</h2>
+            </div>
+            <FirstStage />
+        </div>
+    );
+}
